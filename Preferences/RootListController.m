d99b1b20c16d341665c7a587ebb52b928bfa847a
Nomura Yusuke <149690876+tqmane@users.noreply.github.com>
2026-09-12 11:08:20 +0900

    fix(preferences): retain mutable specifier storage

--- Preferences/RootListController.m
+++ Preferences/RootListController.m
@@ -6,7 +6,7 @@
 @implementation GSRootListController
 - (NSArray *)specifiers{
  if(!_specifiers){PSSpecifier *group=[PSSpecifier emptyGroupSpecifier];[group setProperty:@"Accounts, quality, background restrictions and upload queue are managed by gotohpd." forKey:@"footerText"];
- PSSpecifier *open=[PSSpecifier preferenceSpecifierNamed:@"Open GoToHP settings" target:self set:NULL get:NULL detail:Nil cell:PSButtonCell edit:Nil];open.buttonAction=@selector(openPanel);_specifiers=@[group,open];}return _specifiers;
+ PSSpecifier *open=[PSSpecifier preferenceSpecifierNamed:@"Open GoToHP settings" target:self set:NULL get:NULL detail:Nil cell:PSButtonCell edit:Nil];open.buttonAction=@selector(openPanel);_specifiers=[@[group,open] mutableCopy];}return _specifiers;
 }
 - (void)openPanel{GSPanel *p=[[GSPanel alloc]initWithStyle:UITableViewStyleInsetGrouped];p.settingsMode=YES;[self presentViewController:[[UINavigationController alloc]initWithRootViewController:p] animated:YES completion:nil];}
 @end
